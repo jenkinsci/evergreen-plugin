5a1739afef883ec268ff12d786fa08e5b155fc9b
Baptiste Mathus <batmat@batmat.net>
2018-04-06 12:32:53 +0200

    Use Functions.printThrowable for exception formatting
    As advised by Jesse during JEP review

--- src/main/java/io/jenkins/plugins/essentials/logging/JsonFormatter.java
+++ src/main/java/io/jenkins/plugins/essentials/logging/JsonFormatter.java
@@ -1,5 +1,6 @@
 package io.jenkins.plugins.essentials.logging;
 
+import hudson.Functions;
 import net.sf.json.JSONObject;
 
 import javax.annotation.Nonnull;
@@ -23,17 +24,8 @@ public class JsonFormatter extends Formatter {
 
         final Throwable thrown = record.getThrown();
         if (thrown != null) {
-            jsonLog.put("exception", formatException(thrown));
+            jsonLog.put("exception", Functions.printThrowable(thrown));
         }
         return jsonLog.toString() + System.lineSeparator();
-    }
-
-    private String formatException(@Nonnull Throwable thrown) {
-        StringWriter sw = new StringWriter();
-        PrintWriter pw = new PrintWriter(sw);
-        pw.println();
-        thrown.printStackTrace(pw);
-        pw.close();
-        return sw.toString();
     }
 }
