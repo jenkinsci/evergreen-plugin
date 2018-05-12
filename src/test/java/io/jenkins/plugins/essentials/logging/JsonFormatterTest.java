186ab789241dd41c75bd902cf321d44c9a653871
Baptiste Mathus <batmat@batmat.net>
2018-05-12 14:01:33 +0200

    Check more JEP 304 spec related things
    Especially version was missing: this test should now fail.
    Next commit to fix it.

--- src/test/java/io/jenkins/plugins/essentials/logging/JsonFormatterTest.java
+++ src/test/java/io/jenkins/plugins/essentials/logging/JsonFormatterTest.java
@@ -1,11 +1,16 @@
 package io.jenkins.plugins.essentials.logging;
 
+import net.sf.json.JSONObject;
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.logging.Level;
 import java.util.logging.LogRecord;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+
 public class JsonFormatterTest {
 
     @Test
@@ -15,12 +20,22 @@ public class JsonFormatterTest {
         final LogRecord record = new LogRecord(Level.INFO, msg);
         record.setLoggerName("The name");
         record.setParameters(new String[]{"yay"});
-
         record.setThrown(new IllegalStateException());
 
         final String json = new JsonFormatter().format(record);
-
         assertThat(json).isNotBlank();
+
+        final JSONObject jsonObject = JSONObject.fromObject(json);
+
+        // explicit checks based on https://github.com/jenkinsci/jep/tree/master/jep/304#logging-format
+        assertThat(jsonObject.get("version")).isNotNull();
+        assertThat(jsonObject.get("timestamp")).isNotNull();
+        assertThat(jsonObject.get("name")).isNotNull();
+        assertThat(jsonObject.get("level")).isNotNull();
+        assertThat(jsonObject.get("message")).isNotNull();
+
+        final Object exception = jsonObject.get("exception");
+        assertThat(exception).isNotNull();
         assertThat(json).contains(msg.replaceAll("\n", "\\\\n")
                                           .replaceAll("\\{.*}", ""));
         assertThat(json).contains("yay");
