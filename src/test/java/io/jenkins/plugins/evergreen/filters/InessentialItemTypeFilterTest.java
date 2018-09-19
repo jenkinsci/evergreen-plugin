338f8590ba08dd8b57e3ea10e3637b53e4f36996
Baptiste Mathus <batmat@batmat.net>
2018-09-19 16:59:34 +0200

    Filter out matrix-project too

--- src/test/java/io/jenkins/plugins/evergreen/filters/InessentialItemTypeFilterTest.java
+++ src/test/java/io/jenkins/plugins/evergreen/filters/InessentialItemTypeFilterTest.java
@@ -1,13 +1,14 @@
 package io.jenkins.plugins.evergreen.filters;
 
 import hudson.model.TopLevelItemDescriptor;
-import org.assertj.core.api.Assertions;
 import org.junit.Rule;
 import org.junit.Test;
 import org.jvnet.hudson.test.JenkinsRule;
 
 import java.util.List;
 import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class InessentialItemTypeFilterTest {
     @Rule
@@ -20,10 +21,14 @@ public class InessentialItemTypeFilterTest {
                 .collect(Collectors.toList());
 
         System.out.println(foundTopLevelItems);
-        Assertions.assertThat(foundTopLevelItems).
+
+        System.out.println(j.jenkins.pluginManager.getPlugins());
+
+        assertThat(foundTopLevelItems).
                 doesNotContain(
                         "hudson.model.FreeStyleProject$DescriptorImpl",
-                        "hudson.maven.MavenModuleSet$DescriptorImpl"
+                        "hudson.maven.MavenModuleSet$DescriptorImpl",
+                        "hudson.matrix.MatrixProject$DescriptorImpl"
                 );
     }
 
