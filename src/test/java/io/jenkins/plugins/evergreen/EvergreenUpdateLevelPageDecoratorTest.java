ce505748447994fd2bf1aa358ec179de4b3cfc57
Baptiste Mathus <batmat@batmat.net>
2018-10-30 13:09:03 +0100

    Add assertion to fail more clearly
    if the file is missing for whatever reason

--- src/test/java/io/jenkins/plugins/evergreen/EvergreenUpdateLevelPageDecoratorTest.java
+++ src/test/java/io/jenkins/plugins/evergreen/EvergreenUpdateLevelPageDecoratorTest.java
@@ -3,6 +3,7 @@ package io.jenkins.plugins.evergreen;
 import org.junit.Test;
 
 import java.io.File;
+import java.net.URL;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -10,7 +11,9 @@ public class EvergreenUpdateLevelPageDecoratorTest {
     @Test
     public void getUpdateLevel() {
 
-        final File manifestFile = new File(getClass().getResource("manifest.json").getPath());
+        final URL resource = getClass().getResource("manifest.json");
+        assertThat(resource).isNotNull();
+        final File manifestFile = new File(resource.getPath());
         assertThat(manifestFile).exists();
         assertThat(new EvergreenUpdateLevelPageDecorator(manifestFile).getUpdateLevel()).isEqualTo("2");
     }
